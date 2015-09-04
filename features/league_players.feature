f6fb9c0d8c29f5fe8a8d327c7cce1845c0278c86
Geoff Massanek <gmassanek@gmail.com>
2015-09-03 21:21:33 -0400

    Sort players by points

--- features/league_players.feature
+++ features/league_players.feature
@@ -5,3 +5,9 @@ Feature: Leagues have Players
     When I go to that league's players page
     And I filter the table by "snooki"
     Then I should only see the "Snooki" player
+
+  Scenario: Defaults to the best players
+    Given the BadCeleb League seeds have been run
+    And one team has a bunch of points
+    When I go to that league's players page
+    Then I should see the best player first
--- features/step_definitions/league_players_steps.rb
+++ features/step_definitions/league_players_steps.rb
@@ -8,3 +8,14 @@ Then(/^I should only see the "(.*?)" player$/) do |search_term|
     expect(page).to have_content(search_term)
   end
 end
+
+Then(/^I should see the best player first$/) do
+  first_row = first("table.dataTable tbody tr")
+  within(first_row) do
+    if @cat1.value > @cat2.value + @cat3.value
+      expect(page).to have_content(@player1.name)
+    else
+      expect(page).to have_content(@player2.name)
+    end
+  end
+end
