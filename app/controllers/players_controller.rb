2bef85fd066d54f54b7259b54be5c7ea376ba59b
Geoff Massanek <gmassanek@gmail.com>
2015-09-04 09:53:49 -0400

    Eager loading on player contoller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,6 +7,12 @@ class PlayersController < ApplicationController
 
   private
     def set_league
-      @league = League.find(params[:league_id])
+      @league = League.includes({
+        league_point_categories: [],
+        players: {
+          league_position: [],
+          point_submissions: [:league_point_category]
+        },
+      }).find(params[:league_id])
     end
 end
